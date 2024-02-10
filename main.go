5765feb02dbf6e25c73bac6f7905998eaa847b69
sackvo8z <sackvo8z@gmail.com>
2024-02-10 21:33:15 +0700

    create api, view html by fiber package of go

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("hello go world")
-	fmt.Println("SAck BOUDNASINH")
-	fmt.Println("SAck BOUDNASINH")
-	fmt.Println("SAck BOUDNASINH")
+	app := fiber.New()
+	// app.Get("/", func(c *fiber.Ctx) error {
+	// 	return c.SendString("hello world, this my web sack path /x for value and path /name for yourname")
+	// })
+	app.Static("/", "./public")
+
+	app.Get("/x/:x", func(c *fiber.Ctx) error {
+		return c.SendString("value: " + c.Params("x"))
+	})
+
+	app.Get("/name/:name?", func(c *fiber.Ctx) error {
+		if c.Params("name") != "" {
+			return c.SendString("ສະບາຍດີ " + c.Params("name"))
+		}
+		return c.SendString("ເຈົ້າຈົ່ງບອກຊື່ມາດຽວນີ້ ")
+	})
+	app.Get("/api/*", func(c *fiber.Ctx) error {
+		return c.SendString("api is : " + c.Params("*"))
+
+	})
+
+	app.Listen(":8000")
 }
